4792acfabd3dc341a00820c9441965fa018a1a86
Elcheffe1888 <138306236+Elcheffe1888@users.noreply.github.com>
2026-04-29 22:36:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.use(session({
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
   },
 }));
 
